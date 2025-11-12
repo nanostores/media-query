e476168c6284dfdd35cb74dd255077ba95fbb24f
baileys-li <47776594+baileys-li@users.noreply.github.com>
2025-11-12 18:31:36 +0200

    Refactor fromMediaQuery to use dynamic values

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ import { atom, onMount } from 'nanostores'
 
 export function fromMediaQuery(query, trueValue = true, falseValue = false) {
   if (typeof window === 'undefined' || !window.matchMedia) {
-    return atom(false)
+    return atom(falseValue)
   }
 
   let media = window.matchMedia(query)
-  let store = atom(media.matches ? trueValue : falseValue)
+  let getValue = () => media.matches ? trueValue : falseValue
+  let store = atom(getValue())
 
   function listen() {
-    store.set(media.matches ? trueValue : falseValue)
+    store.set(getValue())
   }
   onMount(store, () => {
     media.addEventListener('change', listen)
