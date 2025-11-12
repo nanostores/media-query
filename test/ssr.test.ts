0f9e73bf7120ec28baeb4768218529cab80fc910
Arthur Baileys Li <arthurlitovko@gmail.com>
2025-11-12 18:44:30 +0200

    Add ssr test with custom value

--- test/ssr.test.ts
+++ test/ssr.test.ts
@@ -7,3 +7,8 @@ test('works outside of browser', () => {
   let store = fromMediaQuery('(max-width: 600px)')
   equal(store.get(), false)
 })
+
+test('Server returns custom false value', () => {
+  let store = fromMediaQuery('(max-width: 600px)', '🍏', '🥔')
+  equal(store.get(), '🥔')
+})
